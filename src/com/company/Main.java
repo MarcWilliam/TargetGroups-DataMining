e3254c66fc9507e31f269ff7403675081fcf6aff
marcwafik <marc.wafik@gmail.com>
2018-05-21 03:50:22 +0200

    improved the print result

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,18 +9,7 @@ import java.util.Arrays;
 
 public class Main {
 
-
     public static void main(String[] args) throws IOException {
-        /*
-        String[] labels = {"something", "somethingelse"};
-
-        NdimPoint data[] = {
-                new NdimPoint(1, 1),
-                new NdimPoint(2, 1),
-                new NdimPoint(1, 4),
-                new NdimPoint(2, 4),
-        };
-        */
 
         Pair<String[], NdimPoint[]> tmp = parseFile("data2.csv");
         String[] labels = tmp.getKey();
@@ -28,12 +17,11 @@ public class Main {
 
 
         Cluster[] clusters = kmean(labels, data, 5);
-
+        PrintClusters(labels, clusters, -1);
     }
 
 
     static Pair<String[], NdimPoint[]> parseFile(String filePath) throws IOException {
-        // <<<<<<<<<<<<<<<<<< Abdelhamed ur job starts here feel free to delete the body just try to keep the function signature the same
         ArrayList<NdimPoint> data = new ArrayList<>(500);
         String labels[] = {};
 
@@ -104,14 +92,39 @@ public class Main {
 
 
     private static void PrintClusters(String labels[], Cluster clusters[], int n) {
-        System.out.println("\n\n============================================= \tLoop " + n + "\t ==============================================");
+
+        String ANSI_RESET = "\u001B[0m";
+        String ANSI_BLACK = "\u001B[30m";
+        String ANSI_RED = "\u001B[31m";
+        String ANSI_GREEN = "\u001B[32m";
+        String ANSI_YELLOW = "\u001B[33m";
+        String ANSI_BLUE = "\u001B[34m";
+        String ANSI_PURPLE = "\u001B[35m";
+        String ANSI_CYAN = "\u001B[36m";
+        String ANSI_WHITE = "\u001B[37m";
+
+        //System.out.print(ANSI_BLUE);
+        if (n == -1) {
+            System.out.println(ANSI_CYAN + "RESULT\tRESULT\tRESULT\tRESULT\tRESULT\tRESULT\tRESULT\tRESULT\tRESULT\tRESULT\tRESULT\tRESULT\tRESULT\tRESULT\tRESULT\tRESULT\t" + ANSI_RESET);
+        } else {
+            System.out.println("\n\n============================================= \tLoop " + n + "\t ==============================================");
+        }
         for (int i = 0; i < clusters.length; i++) {
             for (int j = 0; j < clusters[i].centroid.dims.length; j++) {
-                System.out.print(labels[j] + ": " + clusters[i].centroid.dims[j] + "\t\t");
+                System.out.print(labels[j] + ": " + ANSI_CYAN + Math.round(clusters[i].centroid.dims[j]) + ANSI_RESET + "\t\t");
             }
+
+            String tmp = Arrays.toString(clusters[i].data)
+                    .replace(", ", "\t")
+                    .replace("]", "")
+                    .replace("[", "")
+                    .replace("\tnull", "");
+
+            char[] ar = new char[tmp.length() * 2];
+            Arrays.fill(ar, '-');
             System.out.println();
-            System.out.println(Arrays.toString(clusters[i].data).replace(", ", "\t").replace("]", "").replace("[", ""));
-            System.out.println("-------------------------------------------------------------------------------------------------------");
+            System.out.println(tmp);
+            System.out.println(ar);
         }
 
         System.out.println();
@@ -156,7 +169,6 @@ public class Main {
         }
 
         public NdimPoint div(double div) {
-
             NdimPoint ret = new NdimPoint(new double[this.dims.length]);
             for (int i = 0; i < this.dims.length; i++) {
                 ret.dims[i] = this.dims[i] / div;
